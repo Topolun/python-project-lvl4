e525d3ca3f26bc26a87d3f18ac6b206117cc6da0
Topolun <vik83tu@yandex.ru>
2021-03-15 21:18:13 +0400

    First test added

--- Makefile
+++ Makefile
@@ -1,8 +1,11 @@
-runserver:
-	poetry run python manage.py runserver
-
 lint:
 	poetry run flake8
+
+test:
+	poetry run python manage.py test
+
+runserver:
+	poetry run python manage.py runserver
 
 requirements:
 	poetry export -f requirements.txt -o requirements.txt --without-hashes
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
-# Create your tests here.
+from users.models import CustomUser
+
+
+class CustomUserModelTest(TestCase):
+
+    def test_create_user(self):
+        user = CustomUser(username='test_user')
+        self.assertIs(user.username, 'test_user')
+    
