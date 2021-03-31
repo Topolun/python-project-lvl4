5cdad14c5cdaef5548ecfb658cba29ac59f8f143
Topolun <vik83tu@yandex.ru>
2021-03-31 20:05:41 +0400

    Test for forms is added

--- users/tests.py
+++ users/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 
 from users.models import CustomUser
+from users.forms import CustomUserCreationForm
 
 
 class CustomUserModelTest(TestCase):
@@ -8,3 +9,42 @@ class CustomUserModelTest(TestCase):
     def test_create_user(self):
         user = CustomUser(username='test_user')
         self.assertIs(user.username, 'test_user')
+
+
+class CustomUserCreationFormTest(TestCase):
+
+    def test_create_user_form(self):
+        user_data = {
+            'username': 'test_user',
+            'password1': 'something',
+            'password2': 'something'
+            }
+        user_form = CustomUserCreationForm(user_data)
+        self.assertIs(user_form.is_valid(), True)
+
+    def test_create_wrong_user_form(self):
+        user_data = {
+            'username': 'test_user',
+            'password1': 'something',
+            'password2': 'something_else'
+            }
+        user_form = CustomUserCreationForm(user_data)
+        self.assertIs(user_form.is_valid(), False)
+
+    def test_create_empty_password_user_form(self):
+        user_data = {
+            'username': 'test_user',
+            'password1': '',
+            'password2': ''
+            }
+        user_form = CustomUserCreationForm(user_data)
+        self.assertIs(user_form.is_valid(), False)
+
+    def test_create_empty_username_user_form(self):
+        user_data = {
+            'username': '',
+            'password1': 'something',
+            'password2': 'something'
+            }
+        user_form = CustomUserCreationForm(user_data)
+        self.assertIs(user_form.is_valid(), False)
