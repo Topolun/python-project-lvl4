3ba908c5aef002f1e11ae445b65c16731092b214
Topolun <vik83tu@yandex.ru>
2021-04-17 21:20:55 +0400

    new tests

--- users/tests.py
+++ users/tests.py
@@ -2,6 +2,8 @@ from django.test import TestCase
 
 from users.models import CustomUser
 from users.forms import CustomUserCreationForm
+from django.contrib.auth import authenticate
+from django.contrib.auth import get_user_model
 
 
 class CustomUserModelTest(TestCase):
@@ -48,3 +50,66 @@ class CustomUserCreationFormTest(TestCase):
             }
         user_form = CustomUserCreationForm(user_data)
         self.assertIs(user_form.is_valid(), False)
+
+
+class SigninTest(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username='test',
+            password='123456',
+            )
+        self.user.save()
+
+    def tearDown(self):
+        self.user.delete()
+
+    def test_correct(self):
+        user = authenticate(username='test', password='123456')
+        self.assertTrue((user is not None) and user.is_authenticated)
+
+    def test_wrong_username(self):
+        user = authenticate(username='wrong', password='123456')
+        self.assertFalse(user is not None and user.is_authenticated)
+
+    def test_wrong_pssword(self):
+        user = authenticate(username='test', password='wrong')
+        self.assertFalse(user is not None and user.is_authenticated)
+
+
+class LoginTest(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username='test',
+            password='123456',
+            )
+        self.user.save()
+
+    def tearDown(self):
+        self.user.delete()
+
+    def test_login_success(self):
+        response = self.client.login(username='test', password='123456')
+        self.assertTrue(response)
+
+    def test_login_fail(self):
+        response = self.client.login(username='test', password='wrong')
+        self.assertFalse(response)
+
+
+class UserDataCangesTest(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username='test',
+            password='123456',
+            )
+        self.user.save()
+
+    def tearDown(self):
+        self.user.delete()
+
+    def test_correct(self):
+        self.client.login(username='test', password='123456')
+        newName = 'NewName'
+        self.client.post('/users/1/update/', {'username': newName})
+        resp = self.client.get('/users/1/update/')
+        self.assertEqual(str(resp.context['user']), newName)
